5a19173cf021c806f035693d27f9286494678d55
Abdurraouf Etagiuri <roofa.etagiuri1@gmail.com>
2023-07-26 16:53:08 -0600

    added fake data for reference

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios' //how we access the API
 function App() {
+
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=b1c3290c0706453abdf0a4e989bd6b11'
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>40°C</h1>
+          </div>
+          <div className='desc'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottomer'>
+          <div className="feels">
+            <p>40°C</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12MPH
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
